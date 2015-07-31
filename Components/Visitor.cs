3bd68e3708f9f8b8a407e97578e85f1e85f46428
John Leitch <JohnLeitch@outlook.com>
2015-07-31 01:38:44 -0400

    Added Visitor class.
    Added Visitor class.

---
+++ Components/Visitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    public static class Visitor<TElement>
+    {
+        public static void Visit(
+            TElement group,
+            Action<TElement> action,
+            Func<TElement, IEnumerable<TElement>> getChildren)
+        {
+            action(group);
+
+            foreach (var c in getChildren(group))
+            {
+                Visit(group, action, getChildren);
+            }
+        }
+    }
+}
